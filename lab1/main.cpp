6dbd7b9ec34c78176803ec7a491832bf81f3e7c1
jinifief <jinified@gmail.com>
2016-02-09 17:20:20 +0800

    Added primitive shapes

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -10,6 +10,7 @@
 	#include <OpenGL/gl.h>
 	#include <GLUT/GLUT.h>
 #else
+	#include <GL/glu.h>
 	#include <GL/glut.h>
 #endif
 
@@ -18,6 +19,119 @@ using namespace std;
 float alpha = 0.0, k=1;
 float tx = 0.0, ty=0.0;
 
+
+// Vertex arrays
+
+static GLint vertices[] = {-5, -5, 
+						   -4, 4, 
+						   -3, -3, 
+						   -2, -2};
+
+static GLfloat colors[] = {0.5, 0, 0, 
+						   0, 0.5, 0,
+						   0, 0, 0.5};
+
+// Lighting related
+
+void setLightSource() {
+	GLfloat light_ambient[] = { 0.9, 0.1, 0.1, 1.0 };
+	GLfloat light_diffuse[] = { 0.8, 0.8, 0.8, 1.0 };
+	GLfloat light_specular[] = { 1.0, 0.0, 0.0, 1.0 };
+	GLfloat light_position[] = { 1.0, 1.0, 1.0, 0.0 };
+
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_DEPTH_TEST);
+}
+
+void setMaterialLighting() {
+	// Sets material properties
+	GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat mat_shininess[] = { 90.0 };
+	GLfloat mat_emission[] = {0.9, 0.1, 0.1, 0.0};
+
+	glMaterialfv(GL_FRONT, GL_EMISSION, mat_emission);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+   	glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+}
+
+// Viewing related
+
+void viewTransform() {
+	// (0,0,5) -> camera position. -z value behind image
+	// (0,0,0) -> where camera pointing at
+	// (0,1.0,0) -> up vector
+	gluLookAt (0.0, 0.0, -1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+}
+// Primitive shapes used
+
+void drawSolidTriangle() {
+
+	glPushMatrix();
+		glBegin(GL_TRIANGLES);
+			glNormal3f(0,0,5);
+			glColor3f(1.0, 0, 0);
+			glVertex3f(0,0,0);
+			glNormal3f(0,0,1);
+			glColor3f(1.0, 0, 0);
+			glVertex3f(5,0,0);
+			glNormal3f(0,0,1);
+			glColor3f(1.0, 0, 0);
+			glVertex3f(0,5,0);
+		glEnd();
+	glPopMatrix();
+}
+
+void drawStipleLine() {
+	glPushMatrix();
+		glLineStipple(1, 0x00FF); // Specifies how line pattern
+		glEnable(GL_LINE_STIPPLE);
+		glLineWidth(5.0);		// Cannot be changed in Begin End
+		glBegin(GL_LINE_STRIP);
+			glColor3f(1.0, 1.0, 0);
+			glVertex2f(-5, -1.0);
+			glVertex2f(-3, 1.0);
+			glVertex2f(-1, -2.0);
+			glVertex2f(1, 2.0);
+			glVertex2f(3, -3.0);
+			glVertex2f(5, 3.0);
+		glEnd();
+	glPopMatrix();
+}
+
+void drawZigzag() {
+	glPushMatrix();
+		glLineWidth(20.0);		// Cannot be changed in Begin End
+		glBegin(GL_LINE_STRIP);
+			glColor3f(1.0, 1.0, 0);
+			glVertex2f(-5, -1.0);
+			glVertex2f(-3, 1.0);
+			glVertex2f(-1, -2.0);
+			glVertex2f(1, 2.0);
+			glVertex2f(3, -3.0);
+			glVertex2f(5, 3.0);
+		glEnd();
+	glPopMatrix();
+}
+
+void drawTrianglePoints() {
+	glPushMatrix();
+		glPointSize(10);	// Sets point size
+		glBegin(GL_POINTS);
+			glColor3f(1.0, 0, 0);
+			glVertex2f(-1, 0);
+			glColor3f(0.0, 1.0, 0);
+			glVertex2f(1, 0);
+			glColor3f(0.0, 0, 1.0);
+			glVertex2f(0, 2);
+		glEnd();
+	glPopMatrix();
+}
 
 void display(void)
 {
@@ -31,6 +145,7 @@ void display(void)
 	glRotatef(alpha, 0, 0, 1);
 	
 	//draw your stuff here
+	drawSolidTriangle();
 	glPopMatrix();
 	glFlush ();
 }
@@ -49,11 +164,10 @@ void reshape (int w, int h)
 
 void init(void)
 {
-	glClearColor (1.0, 1.0, 1.0, 1.0);
+	// setSingleLighting();
+	glClearColor (0.0, 0.0, 0.0, 0.0);
 	glShadeModel (GL_SMOOTH);
 }
-
-
 
 void keyboard (unsigned char key, int x, int y)
 {
