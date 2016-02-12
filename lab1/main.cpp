1f6f3bb7c3ff08a8f3fefcbd3b6ce460593c4da7
jinifief <jinified@gmail.com>
2016-02-12 20:35:15 +0800

    Completed lab1 draft 1

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,5 +1,6 @@
 // CS3241Lab1.cpp : Defines the entry point for the console application.
 #include <cmath>
+#include <math.h>
 #include <iostream>
 
 /* Include header files depending on platform */
@@ -19,118 +20,65 @@ using namespace std;
 float alpha = 0.0, k=1;
 float tx = 0.0, ty=0.0;
 
-
-// Vertex arrays
-
-static GLint vertices[] = {-5, -5, 
-						   -4, 4, 
-						   -3, -3, 
-						   -2, -2};
-
-static GLfloat colors[] = {0.5, 0, 0, 
-						   0, 0.5, 0,
-						   0, 0, 0.5};
+// Colors
+GLfloat color[][4] = { 
+	{0.3, 0, 0.8, 0.75},
+	{0.8, 0, 0.3, 0.75}
+};
 
 // Lighting related
 
-void setLightSource() {
-	GLfloat light_ambient[] = { 0.9, 0.1, 0.1, 1.0 };
-	GLfloat light_diffuse[] = { 0.8, 0.8, 0.8, 1.0 };
-	GLfloat light_specular[] = { 1.0, 0.0, 0.0, 1.0 };
-	GLfloat light_position[] = { 1.0, 1.0, 1.0, 0.0 };
-
-    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-    glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
-    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-
-	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT0);
-	glEnable(GL_DEPTH_TEST);
+void setBlending() {
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 }
 
-void setMaterialLighting() {
-	// Sets material properties
-	GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
-	GLfloat mat_shininess[] = { 90.0 };
-	GLfloat mat_emission[] = {0.9, 0.1, 0.1, 0.0};
-
-	glMaterialfv(GL_FRONT, GL_EMISSION, mat_emission);
-	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
-   	glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
-}
-
-// Viewing related
-
-void viewTransform() {
-	// (0,0,5) -> camera position. -z value behind image
-	// (0,0,0) -> where camera pointing at
-	// (0,1.0,0) -> up vector
-	gluLookAt (0.0, 0.0, -1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-}
 // Primitive shapes used
 
-void drawSolidTriangle() {
+void drawPoint(float x, float y, int size) {
+	glPointSize(size);	// Sets point size
+	glBegin(GL_POINTS);
+		glColor3f(1.0, 1.0, 0.0);
+		glVertex2f(x, y);
+	glEnd();
+}
 
+void drawSinglePillar(GLfloat *color, float width, float height, float begin, int size) {
+	float top = begin + height;
+	glBegin(GL_POLYGON);
+		glColor4f(color[0], color[1], color[2], color[3]);
+		glVertex2f(-width, begin);
+		glVertex2f(-width, top -3);
+		glVertex2f(0, top);
+		glVertex2f(width, top -3);
+		glVertex2f(width, begin);
+	glEnd();
+	drawPoint(0.0, top, size);
+}
+
+void drawThrone() {
+	int SIZE = 15;
+	float WIDTH = 0.8;
+	float BEGIN = -8;
+	float MIN_HEIGHT = 2;
 	glPushMatrix();
-		glBegin(GL_TRIANGLES);
-			glNormal3f(0,0,5);
-			glColor3f(1.0, 0, 0);
-			glVertex3f(0,0,0);
-			glNormal3f(0,0,1);
-			glColor3f(1.0, 0, 0);
-			glVertex3f(5,0,0);
-			glNormal3f(0,0,1);
-			glColor3f(1.0, 0, 0);
-			glVertex3f(0,5,0);
-		glEnd();
+		for (int i=6; i > 0; i--) {
+			drawSinglePillar(color[i%2], WIDTH, MIN_HEIGHT + (i*2), BEGIN, SIZE);
+			WIDTH-= 0.1;
+			SIZE-= 2;
+			glTranslatef(-1.3, 0, 0);
+			glRotatef(8.0, 0, 0, 1);
+		}
 	glPopMatrix();
 }
 
-void drawStipleLine() {
+void drawFullThrone() {
 	glPushMatrix();
-		glLineStipple(1, 0x00FF); // Specifies how line pattern
-		glEnable(GL_LINE_STIPPLE);
-		glLineWidth(5.0);		// Cannot be changed in Begin End
-		glBegin(GL_LINE_STRIP);
-			glColor3f(1.0, 1.0, 0);
-			glVertex2f(-5, -1.0);
-			glVertex2f(-3, 1.0);
-			glVertex2f(-1, -2.0);
-			glVertex2f(1, 2.0);
-			glVertex2f(3, -3.0);
-			glVertex2f(5, 3.0);
-		glEnd();
+		drawThrone();
+		glScalef(-1, 1, 1);
+		drawThrone();
 	glPopMatrix();
-}
 
-void drawZigzag() {
-	glPushMatrix();
-		glLineWidth(20.0);		// Cannot be changed in Begin End
-		glBegin(GL_LINE_STRIP);
-			glColor3f(1.0, 1.0, 0);
-			glVertex2f(-5, -1.0);
-			glVertex2f(-3, 1.0);
-			glVertex2f(-1, -2.0);
-			glVertex2f(1, 2.0);
-			glVertex2f(3, -3.0);
-			glVertex2f(5, 3.0);
-		glEnd();
-	glPopMatrix();
-}
-
-void drawTrianglePoints() {
-	glPushMatrix();
-		glPointSize(10);	// Sets point size
-		glBegin(GL_POINTS);
-			glColor3f(1.0, 0, 0);
-			glVertex2f(-1, 0);
-			glColor3f(0.0, 1.0, 0);
-			glVertex2f(1, 0);
-			glColor3f(0.0, 0, 1.0);
-			glVertex2f(0, 2);
-		glEnd();
-	glPopMatrix();
 }
 
 void display(void)
@@ -145,7 +93,7 @@ void display(void)
 	glRotatef(alpha, 0, 0, 1);
 	
 	//draw your stuff here
-	drawSolidTriangle();
+	drawFullThrone();
 	glPopMatrix();
 	glFlush ();
 }
@@ -164,7 +112,7 @@ void reshape (int w, int h)
 
 void init(void)
 {
-	// setSingleLighting();
+	setBlending();
 	glClearColor (0.0, 0.0, 0.0, 0.0);
 	glShadeModel (GL_SMOOTH);
 }
